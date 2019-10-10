fa89279b3cd2d8d3c526d23256da568e7767cba7
Dhruv Gupta <guptadhruv2803@gmail.com>
2019-10-10 19:51:50 +0530

    java commit

---
+++ Fitso_java/app/src/main/java/com/example/fitso/UserActivity.java
@@ -0,0 +1,36 @@
+
+package com.example.fitso;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserActivity extends Activity {
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(UserActivity.this, ActivityLogin.class);
+                startActivity(I);
+                //edited later
+                finish();
+
+            }
+        });
+
+    }
+}
